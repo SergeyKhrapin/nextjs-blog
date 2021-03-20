571274478cc2dc1823eb29d9b9584454f8ec99fa
SergeyKhrapin <khrapins@gmail.com>
2021-03-20 17:59:32 +0200

    Head component

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,10 +1,14 @@
+import Head from 'next/head'
 import Link from 'next/link'
 
-console.log('FIRST_POST');
+console.log('FIRST_POST')
 
 export default function FirstPost() {
     return (
         <>
+            <Head>
+                <title>First post</title>
+            </Head>
             <h1>First Post</h1>
             <Link href="/">
                 <a>Back to Home</a>
